b6dbb2efcb367bbd40437a6935cf650f206e8c80
sanjay <ssah6694@gmail.com>
2022-03-08 14:32:24 +0530

    [sanjay]Added: Employee Absent or Present

---
+++ MyEmployee/MyEmployee/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class class1
+    {
+        public void Employee()
+        {
+            int Is_Full_Time = 1;
+            Random obj = new Random();
+            int empCheck = obj.Next(0, 2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+           
+    }
+
---
+++ MyEmployee/MyEmployee/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyEmployee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            class1 obj = new class1();
+            obj.Employee();
+        }
+    }
+}
